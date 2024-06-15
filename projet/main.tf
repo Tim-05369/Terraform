1ab690cf7f3d00d45381f062f885af9246b73ca8
Tim GIMENEZ <timgim2000@hotmail.fr>
2024-06-15 19:32:44 +0200

    feat: ajout du trigger

--- projet/main.tf
+++ projet/main.tf
@@ -1,7 +1,7 @@
 
 variable "hosts" {
     default = {
-        "127.0.0.2" = "localhost gitlab.local"
+        "127.0.0.2" = "localhost gitlab.local gitlab.me"
         "192.169.1.168" = "gitlab.test"
         "192.169.1.170" = "prometheus.test"
     }
@@ -9,6 +9,9 @@ variable "hosts" {
 
 resource "null_resource" "hosts" {
     for_each = var.hosts
+    triggers = {
+        foo = each.value
+    }
     provisioner "local-exec" {
         command = "echo '${each.key} ${each.value}' >> hosts.txt"
     }
